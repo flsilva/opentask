d25efad126f564e659a28b12ac0ac3ce8b0f5680
Flavio Silva <flsilva7@gmail.com>
2023-07-05 09:20:26 -0300

    (fix) HeroCopy should use ClassNamePropsOptional instead of ClassNameProps.

--- src/app/(marketing)/ui/HeroCopy.tsx
+++ src/app/(marketing)/ui/HeroCopy.tsx
@@ -1,6 +1,6 @@
 import { ChildrenProps } from './ChildrenProps';
-import { ClassNameProps } from './ClassNameProps';
+import { ClassNamePropsOptional } from './ClassNameProps';
 
-export default function HeroCopy({ children, className }: ChildrenProps & ClassNameProps) {
+export default function HeroCopy({ children, className }: ChildrenProps & ClassNamePropsOptional) {
   return <p className={`mt-6 text-xl leading-8 text-gray-500 ${className}`}>{children}</p>;
 }
