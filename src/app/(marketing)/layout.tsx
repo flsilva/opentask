8deba132101d5bee6e5edc7aac43eb3616cde6b6
Flavio Silva <flsilva7@gmail.com>
2023-07-05 11:36:44 -0300

    (feat) add footer always at the bottom.

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,11 +1,13 @@
 import '../globals.css';
 import Header from './ui/Header';
+import Footer from './ui/Footer';
 
 export default function MarketingLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="bg-white">
+    <div className="flex h-screen flex-col bg-white">
       <Header />
       {children}
+      <Footer />
     </div>
   );
 }
---
+++ src/app/(marketing)/ui/Footer.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import { GitHubLogoSvg } from '@/shared/ui/GitHubLogoSvg';
+import { TwitterLogoSvg } from '@/shared/ui/TwitterLogoSvg';
+
+export default function Footer() {
+  return (
+    <footer className="mt-auto">
+      <div className="mb-6 items-center px-6 md:flex md:justify-between lg:px-8">
+        <p className="text-center md:text-left">
+          <Link href="/" className=" hover:text-green-500">
+            © 2023 OpenTask Developers
+          </Link>
+        </p>
+        <div className="mt-6 flex justify-center md:mt-0">
+          <Link className="mr-6" href="/">
+            <GitHubLogoSvg className="fill-black hover:fill-green-500" />
+          </Link>
+          <Link href="/">
+            <TwitterLogoSvg className="fill-black hover:fill-green-500" />
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
+}
