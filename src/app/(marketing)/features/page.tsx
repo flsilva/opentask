34b19382779ba7d311089492b4a0facea39c2455
Flavio Silva <flsilva7@gmail.com>
2023-07-05 08:56:37 -0300

    (refactor): refactor features page into reusable components.

--- src/app/(marketing)/features/page.tsx
+++ src/app/(marketing)/features/page.tsx
@@ -1,29 +1,22 @@
 import 'server-only';
-import Link from 'next/link';
+import Button from '../ui/Button';
+import HeroShell from '../ui/HeroShell';
+import HeroHeading from '../ui/HeroHeading';
+import HeroCopy from '../ui/HeroCopy';
 
-export default function Home() {
+export default function Features() {
   return (
-    <div className="relative isolate px-6 pt-14 lg:px-8">
-      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-        <div className="text-center">
-          <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-6xl sm:leading-tight">
-            Capture Everything <br />
-            <span className="text-green-700">Free Your Mind</span>
-          </h1>
-          <p className="mt-6 text-xl leading-8 text-gray-500">
-            Plan and structure everything, from chores to great ideas, as manageable projects and
-            tasks.
-          </p>
-          <div className="mt-10 flex items-center justify-center">
-            <Link
-              href="/"
-              className="rounded-md bg-green-600 px-3.5 py-2.5 text-sm font-medium text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
-            >
-              Get Started
-            </Link>
-          </div>
-        </div>
+    <HeroShell>
+      <HeroHeading>
+        Capture Everything <br />
+        <span className="text-green-700">Free Your Mind</span>
+      </HeroHeading>
+      <HeroCopy>
+        Plan and structure everything, from chores to great ideas, as manageable projects and tasks.
+      </HeroCopy>
+      <div className="mt-10 flex items-center justify-center">
+        <Button href="/">Get Started</Button>
       </div>
-    </div>
+    </HeroShell>
   );
 }
