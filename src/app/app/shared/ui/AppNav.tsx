8d838edf016ca58516fa79079d76eb4da86d0e72
Flavio Silva <flsilva7@gmail.com>
2023-07-11 16:32:01 -0300

    (feat): implement functional buttons on AppNav (UI only).

--- src/app/app/shared/ui/AppNav.tsx
+++ src/app/app/shared/ui/AppNav.tsx
@@ -1,9 +1,10 @@
 import 'client-only';
 import { useLayoutEffect, useState } from 'react';
+import { usePathname, useRouter } from 'next/navigation';
 import { CalendarTodayIcon } from '@/app/shared/ui/icon/CalendarTodayIcon';
 import { PlusSignalIcon } from '@/app/shared/ui/icon/PlusSignalIcon';
 import { ProjectsIcon } from '@/app/shared/ui/icon/ProjectsIcon';
-import { ProjectData } from '../../project/ProjectData';
+import { ProjectData } from '../../shared/project/ProjectData';
 
 interface AppNavProps {
   readonly isOpen: boolean | null;
@@ -13,50 +14,82 @@ interface AppNavProps {
 
 export default function AppNav({ isOpen, onNewProjectClick, projects }: AppNavProps) {
   const navClassesCommon =
-    'flex flex-col transition-all duration-500 h-full overflow-y-auto overflow-x-hidden bg-gray-50 py-8 px-6 w-[16rem]';
-  const [navClasses, setNavClasses] = useState(`${navClassesCommon} md:ml-0 -ml-[16rem]`);
+    'flex flex-col transition-all duration-500 h-full overflow-y-auto overflow-x-hidden bg-gray-50 px-4 py-4 w-80';
+  const [navClasses, setNavClasses] = useState(`${navClassesCommon} md:ml-0 -ml-80`);
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const activeClassName = 'bg-gray-200';
+
+  const isActive = (item: string) => pathname.lastIndexOf(item) !== -1;
+
+  const onTodayClick = () => {
+    router.push('/app/today');
+  };
+
+  const onProjectsClick = () => {
+    router.push('/app/projects/active');
+  };
+
+  const onProjectClick = (project: ProjectData) => {
+    router.push(`/app/project/${project.id}`);
+  };
 
   useLayoutEffect(() => {
     if (isOpen === null) return;
     let navClasses = navClassesCommon;
-    /*
-    navClasses += isOpen
-      ? ' py-8 w-full min-w-[16rem] max-w-[16rem] px-6'
-      : ' !w-0 !min-w-0 !max-w-0 !px-0';
-      */
-    navClasses += isOpen ? ' ml-0' : ' -ml-[16rem]';
+    navClasses += isOpen ? ' ml-0' : ' -ml-80';
     setNavClasses(navClasses);
   }, [isOpen]);
 
   return (
     <nav className={navClasses}>
-      <div className="mb-6 flex">
+      <button
+        type="button"
+        className={`flex rounded-md p-2 text-sm font-medium text-gray-600 hover:bg-gray-200 ${
+          isActive('today') ? activeClassName : ''
+        }`}
+        onClick={onTodayClick}
+      >
         <CalendarTodayIcon className="fill-gray-600" />
         <div className="ml-2 flex grow items-center justify-between">
-          <p className="text-sm font-medium text-gray-600">Today</p>
+          Today
           <p className="mr-1.5 text-sm font-medium text-gray-400">7</p>
         </div>
+      </button>
+      <div className="mt-4 flex justify-between">
+        <button
+          type="button"
+          className={`flex grow rounded-md p-2 text-sm font-medium text-gray-600 hover:bg-gray-200 ${
+            isActive('projects') ? activeClassName : ''
+          }`}
+          onClick={onProjectsClick}
+        >
+          <ProjectsIcon className="fill-gray-600" />
+          <div className="ml-2 flex grow items-center justify-between">Projects</div>
+        </button>
+        <button
+          type="button"
+          className="rounded-md p-2 text-gray-700 hover:bg-gray-200"
+          onClick={onNewProjectClick}
+        >
+          <span className="sr-only">Open menu</span>
+          <PlusSignalIcon className="fill-gray-600" />
+        </button>
       </div>
-      <div className="mb-4 flex">
-        <ProjectsIcon className="fill-gray-600" />
-        <div className="ml-2 flex grow items-center justify-between">
-          <p className="text-sm font-medium text-gray-600">Projects</p>
+      <nav className="flex flex-col pl-2">
+        {projects.map((project) => (
           <button
+            key={project.id}
             type="button"
-            className="-m-2.5 rounded-md p-2.5 text-gray-700"
-            onClick={onNewProjectClick}
+            className={`flex grow items-center justify-between rounded-md p-2.5 text-sm font-medium text-gray-600 hover:bg-gray-200 ${
+              isActive(`project/${project.id}`) ? activeClassName : ''
+            }`}
+            onClick={() => onProjectClick(project)}
           >
-            <span className="sr-only">Open menu</span>
-            <PlusSignalIcon className="fill-gray-600" />
-          </button>
-        </div>
-      </div>
-      <nav className="pl-2">
-        {projects.map((item) => (
-          <div key={item.id} className="mt-3 flex items-center justify-between">
-            <p className="text-sm text-gray-800">{item.name}</p>
+            <p>{project.name}</p>
             <p className="mr-1.5 text-sm font-medium text-gray-400">3</p>
-          </div>
+          </button>
         ))}
       </nav>
     </nav>
