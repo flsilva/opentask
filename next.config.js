a4b8b8f7d51468ede81c3b3a49fdfae61ee3d1eb
Flavio Silva <flsilva7@gmail.com>
2023-08-11 13:57:21 -0300

    build: add dependencies and config necessary to use mdx

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+      mdxRs: true,
       serverActions: true,
   },
 }
 
-module.exports = nextConfig
+const withMDX = require('@next/mdx')()
+module.exports = withMDX(nextConfig)
