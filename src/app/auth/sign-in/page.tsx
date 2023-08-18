7015f35e954362bbf62f9aa72816a75059895030
Flavio Silva <flsilva7@gmail.com>
2023-08-18 09:51:24 -0300

    feat(auth): remove email OTP sign in option for better UX on mobile PWA

--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -3,13 +3,9 @@ import { redirect } from 'next/navigation';
 import Link from 'next/link';
 import { createServerActionClient } from '@supabase/auth-helpers-nextjs';
 import { Database } from '@/lib/database.types';
-import {
-  buttonClassNameGreen,
-  buttonClassNameWhite,
-} from '@/app/shared/ui//button/buttonClassName';
+import { buttonClassNameWhite } from '@/app/shared/ui//button/buttonClassName';
 import { ChildrenProps } from '@/app/shared/ui/ChildrenProps';
 import { GitHubLogoIcon } from '@/app/shared/ui/icon/GitHubLogoIcon';
-import { GoogleLogoIcon } from '@/app/shared/ui/icon/GoogleLogoIcon';
 import { LinkedInInLogoIcon } from '@/app/shared/ui/icon/LinkedInInLogoIcon';
 import { TwitterLogoIcon } from '@/app/shared/ui/icon/TwitterLogoIcon';
 
@@ -31,29 +27,6 @@ export default async function SignIn() {
   if (session) redirect('/app/today');
   /**/
 
-  const redirectTo = `${process.env.NEXT_PUBLIC_URL}/auth/callback`;
-
-  const signInWithEmailHandler = async (formData: FormData) => {
-    'use server';
-    const email = String(formData.get('email'));
-
-    const supabase = createServerActionClient<Database>({ cookies });
-
-    const { data, error } = await supabase.auth.signInWithOtp({
-      email,
-      options: {
-        emailRedirectTo: redirectTo,
-      },
-    });
-
-    if (error) {
-      console.log(error);
-      throw new Error('There was an error trying to sign you in.');
-    } else {
-      redirect(`/auth/sign-in/check-email-link?email=${email}`);
-    }
-  };
-
   const signInWithOAuth = async (formData: FormData) => {
     'use server';
     const provider = String(formData.get('provider')) as Provider;
@@ -62,7 +35,7 @@ export default async function SignIn() {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider,
       options: {
-        redirectTo,
+        redirectTo: `${process.env.NEXT_PUBLIC_URL}/auth/callback`,
       },
     });
 
@@ -98,26 +71,6 @@ export default async function SignIn() {
           <LinkedInInLogoIcon />
           Continue with LinkedIn
         </OAuthProviderButton>
-        <div className="relative flex items-center py-6">
-          <div className="flex-grow border-t border-gray-200"></div>
-          <p className="mx-4 flex-shrink text-gray-400">Or</p>
-          <div className="flex-grow border-t border-gray-200"></div>
-        </div>
-        <form action={signInWithEmailHandler}>
-          <input
-            id="email"
-            name="email"
-            type="email"
-            autoComplete="email"
-            placeholder="Enter your email"
-            className="mb-6 block w-full rounded-md border border-gray-400 py-1.5 text-gray-900 ring-0 placeholder:text-gray-400 focus:border-gray-900 focus:outline-0 focus:ring-0"
-            required
-            data-lpignore="true"
-          />
-          <button type="submit" className={`${buttonClassNameGreen} flex w-full justify-center`}>
-            Continue with Email
-          </button>
-        </form>
         <p className="mt-6 text-xs text-gray-600">
           You agree to our{' '}
           <Link href="/terms" className=" hover:text-green-500 underline">
