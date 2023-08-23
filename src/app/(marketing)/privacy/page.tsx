486d42d447c72b1a26c1d3b171e586ade8946f0c
Flavio Silva <flsilva7@gmail.com>
2023-08-23 14:53:01 -0300

    legal: minor UI change

--- src/app/(marketing)/privacy/page.tsx
+++ src/app/(marketing)/privacy/page.tsx
@@ -3,8 +3,8 @@ import PrivacyMDX from './privacy.mdx';
 
 export default function About() {
   return (
-    <div className="relative isolate px-6 pt-14 lg:px-8">
-      <div className="mx-auto max-w-2xl mb-20 pt-12 sm:pt-16 lg:pt-20">
+    <div className="relative isolate px-6 lg:px-8">
+      <div className="mx-auto max-w-2xl mb-20 pt-12 sm:pt-20 lg:pt-20">
         <PrivacyMDX />
       </div>
     </div>
