e2c230642a29489a6b3b74205e3020c0073b57c0
Flavio Silva <flsilva7@gmail.com>
2023-08-24 17:23:48 -0300

    marketing: improve copy.

--- src/app/(marketing)/about/page.tsx
+++ src/app/(marketing)/about/page.tsx
@@ -2,38 +2,18 @@ import 'server-only';
 import HeroShell from '../modules/common/HeroShell';
 import HeroHeading from '../modules/common/HeroHeading';
 import HeroCopy from '../modules/common/HeroCopy';
+import AboutMDX from './about.mdx';
 
 export default function About() {
   return (
     <HeroShell className="pt-12 sm:pt-20 lg:pt-20">
       <HeroHeading>
-        Create a community
+        Foster a community
         <br />
         <span className="text-green-700">To learn new tech</span>
       </HeroHeading>
       <HeroCopy className="text-left">
-        OpenTask is a free and open source project created by Flavio Silva on top of the new Next.js
-        App Router (v16.4+) and the unique features it brings together (RSC, Streaming, Server
-        Actions, etc.).
-        <br />
-        <br />
-        The beta version is fully functional, and you can use it as your primary task management
-        tool.
-        <br />
-        <br />
-        The goal is to create a community around the project by making it free and open source,
-        where developers can help evolve the project through contributions for new features and bug
-        fixes.
-        <br />
-        <br />
-        And most importantly, the project has the potential to serve as a central hub for developers
-        to share knowledge and establish best practices for using these new features. Let&apos;s
-        make this an opportunity to give back, learn and evolve.
-        <br />
-        <br />
-        Flavio Silva,
-        <br />
-        July 2023
+        <AboutMDX />
       </HeroCopy>
     </HeroShell>
   );
--- src/app/(marketing)/pricing/page.tsx
+++ src/app/(marketing)/pricing/page.tsx
@@ -9,14 +9,13 @@ export default function Pricing() {
   return (
     <HeroShell className="pt-16 sm:pt-32 lg:pt-38">
       <HeroHeading>
-        Free & Open Source
+        Free &
         <br />
-        <span className="text-green-700">Forever</span>
+        <span className="text-green-700">Open Source</span>
       </HeroHeading>
       <HeroCopy>
-        OpenTask will always be free and open source software. Our goal is not to make money with
-        the project but to build a community and learn together while having fun building a fully
-        functional web app on top of bleeding-edge technology.
+        OpenTask is free and open source software. We aim to build a community and learn together
+        while having fun building a fully functional web app on top of bleeding-edge technology.
         <br />
         <br />
         Check out our{' '}
