c1de002be0669ac7e10361ff67726d1569c3d3bc
Flavio Silva <flsilva7@gmail.com>
2023-08-23 22:08:15 -0300

    feat: improve Features page with new copy and screenshot images

--- src/app/(marketing)/features/page.tsx
+++ src/app/(marketing)/features/page.tsx
@@ -1,9 +1,16 @@
 import 'server-only';
+import Image from 'next/image';
 import Link from 'next/link';
 import { buttonClassNameGreen } from '@/app/modules/common//button/buttonClassName';
 import HeroShell from '../modules/common/HeroShell';
 import HeroHeading from '../modules/common/HeroHeading';
 import HeroCopy from '../modules/common/HeroCopy';
+import imgFeatures01 from './features-img-01.png';
+import imgFeatures02 from './features-img-02.png';
+import imgFeatures03 from './features-img-03.png';
+import imgFeatures04 from './features-img-04.png';
+import imgFeatures05 from './features-img-05.png';
+import imgFeatures06 from './features-img-06.png';
 
 export default function Features() {
   return (
@@ -15,6 +22,94 @@ export default function Features() {
       <HeroCopy>
         Plan and structure everything, from chores to great ideas, as manageable projects and tasks.
       </HeroCopy>
+      <p className="mt-16 mb-8 text-2xl font-medium text-gray-800">Projects</p>
+      <Image
+        alt="Projects on OpenTask"
+        src={imgFeatures01}
+        style={{
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          maxWidth: '280px',
+          width: '100%',
+          height: 'auto',
+        }}
+      />
+      <p className="mt-6 mb-8 text-lg leading-8 text-gray-800">
+        Your projects are your goals. Have them at the appropriate size so you can complete all
+        their tasks and achieve your goals.
+      </p>
+      <p className="mt-16 mb-8 text-2xl font-medium text-gray-800">Tasks</p>
+      <Image
+        alt="Tasks on OpenTask"
+        src={imgFeatures02}
+        style={{
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          maxWidth: '280px',
+          width: '100%',
+          height: 'auto',
+        }}
+      />
+      <p className="mt-6 mb-16 text-lg leading-8 text-gray-800">
+        Your tasks are grouped into projects.
+      </p>
+      <Image
+        alt="Tasks on OpenTask"
+        src={imgFeatures03}
+        style={{
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          maxWidth: '280px',
+          width: '100%',
+          height: 'auto',
+        }}
+      />
+      <p className="mt-6 mb-16 text-lg leading-8 text-gray-800">
+        Break your to-dos into smaller, actionable tasks.
+      </p>
+      <Image
+        alt="Tasks on OpenTask"
+        src={imgFeatures04}
+        style={{
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          maxWidth: '280px',
+          width: '100%',
+          height: 'auto',
+        }}
+      />
+      <p className="mt-6 mb-16 text-lg leading-8 text-gray-800">
+        Set due dates for each task to keep track of them.
+      </p>
+      <Image
+        alt="Tasks on OpenTask"
+        src={imgFeatures05}
+        style={{
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          maxWidth: '280px',
+          width: '100%',
+          height: 'auto',
+        }}
+      />
+      <p className="mt-6 text-lg leading-8 text-gray-800">
+        Complete one task at a time towards your progress.
+      </p>
+      <p className="mt-12 mb-6 text-2xl font-medium text-gray-800">What about today?</p>
+      <Image
+        alt="Tasks on OpenTask"
+        src={imgFeatures06}
+        style={{
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          maxWidth: '280px',
+          width: '100%',
+          height: 'auto',
+        }}
+      />
+      <p className="mt-6 mb-16 text-lg leading-8 text-gray-800">
+        See what tasks you have for today and what is overdue.
+      </p>
       <div className="mt-10 flex items-center justify-center">
         <Link href="/auth/sign-in" className={buttonClassNameGreen}>
           Get Started
