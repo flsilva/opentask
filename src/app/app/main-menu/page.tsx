5ccbf696489da3f0413d8c3d97ad83720cd296de
Flavio Silva <flsilva7@gmail.com>
2023-11-19 12:08:58 -0300

    cleanup: delete unused code

--- src/app/app/main-menu/page.tsx
+++ src/app/app/main-menu/page.tsx
@@ -1,7 +1,5 @@
 import 'server-only';
-import { Dialog } from '@/modules/shared/dialog/Dialog';
 import { MainMenu } from '@/modules/app/shared/main-menu/MainMenu';
-import { RouterActions } from '@/modules/shared/router/RouterActions';
 
 export default function MainMenuPage() {
   return <MainMenu className="mt-8" />;
