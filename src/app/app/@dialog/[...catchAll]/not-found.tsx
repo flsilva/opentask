a6d77c8550012eadd90e66f826a0a766c2c09640
Flavio Silva <flsilva7@gmail.com>
2024-01-23 12:24:58 -0300

    fix: catch-all build error on localhost after upgrading Next to 14.1
    Error: Catch-all must be the last part of the URL

--- src/app/app/@dialog/[...catchAll]/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="flex flex-col items-center mt-8">
-      <h2 className="text-3xl">404</h2>
-      <p className="mt-4">This page could not be found.</p>
-    </div>
-  );
-}
