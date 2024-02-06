ca1dfa5381aff9c20ffbcc9834f9eda3050beb25
Flavio Silva <flsilva7@gmail.com>
2024-02-06 10:02:32 -0300

    refactor: arrow function return component

--- src/features/marketing/shared/ui/HeroCopy.tsx
+++ src/features/marketing/shared/ui/HeroCopy.tsx
@@ -2,6 +2,6 @@ import { twMerge } from 'tailwind-merge';
 import { ChildrenProps } from '@/features/shared/ui/ChildrenProps';
 import { ClassNamePropsOptional } from '@/features/shared/ui/ClassNameProps';
 
-export const HeroCopy = ({ children, className }: ChildrenProps & ClassNamePropsOptional) => {
-  return <p className={twMerge('mt-6 text-xl leading-8 text-gray-800', className)}>{children}</p>;
-};
+export const HeroCopy = ({ children, className }: ChildrenProps & ClassNamePropsOptional) => (
+  <p className={twMerge('mt-6 text-xl leading-8 text-gray-800', className)}>{children}</p>
+);
--- src/features/marketing/shared/ui/HeroHeading.tsx
+++ src/features/marketing/shared/ui/HeroHeading.tsx
@@ -1,9 +1,7 @@
 import { ChildrenProps } from '@/features/shared/ui/ChildrenProps';
 
-export const HeroHeading = ({ children }: ChildrenProps) => {
-  return (
-    <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-6xl sm:leading-tight">
-      {children}
-    </h1>
-  );
-};
+export const HeroHeading = ({ children }: ChildrenProps) => (
+  <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-6xl sm:leading-tight">
+    {children}
+  </h1>
+);
